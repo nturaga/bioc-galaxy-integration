b04888de8ecde4524649537ab8c553f604fb9a85
Mallory Freeberg <malloryfreeberg@users.noreply.github.com>
2016-10-27 09:15:24 -0400

    Grammar changes

--- my_r_tool/my_r_tool.R
+++ my_r_tool/my_r_tool.R
@@ -1,19 +1,22 @@
-## How to run tool
+## How to execute this tool
 # $Rscript my_r_tool.R --input input.csv --output output.csv
 
-# Setup R error handling to go to stderr
-options(show.error.messages=F, error=function(){cat(geterrmessage(),file=stderr());q("no",1,F)})
-# We need to not crash galaxy with an UTF8 error on German LC settings.
+# Send R errors to stderr
+options(show.error.messages = F, error = function(){cat(geterrmessage(), file = stderr()); q("no", 1, F)})
+
+# Avoid crashing Galaxy with an UTF8 error on German LC settings
 loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
 
 # Import library
 library("getopt")
+
 options(stringAsfactors = FALSE, useFancyQuotes = FALSE)
+
 # Take in trailing command line arguments
 args <- commandArgs(trailingOnly = TRUE)
 
-# get options, using the spec as defined by the enclosed list.
-# we read the options from the default: commandArgs(TRUE).
+# Get options using the spec as defined by the enclosed list
+# Options are read from the default: commandArgs(TRUE)
 option_specification = matrix(c(
   'input', 'i', 2, 'character',
   'output', 'o', 2, 'character'
@@ -22,18 +25,17 @@ option_specification = matrix(c(
 # Parse options
 options = getopt(option_specification);
 
-# Print options to see what is going on
-cat("\n input: ",options$input)
-cat("\n output: ",options$output)
+# Print options to stderr for debugging
+cat("\n input: ", options$input)
+cat("\n output: ", options$output)
 
+# Read in the input file
+inp = read.csv(file = options$input, stringsAsFactors = FALSE)
 
-# READ in your input file
-inp = read.csv(file=options$input, stringsAsFactors = FALSE)
-
-# Do something with you input
-# This one changes every value in the first column to 0
+# Changes every value in the first column to 0
 inp$V1 = c(rep(0,10))
 
-# Save your output as the file you want to Galaxy to recognize.
-write.csv(inp, file=options$output,row.names = FALSE)
+# Write output to new file which will be recognized by Galaxy
+write.csv(inp, file = options$output,row.names = FALSE)
+
 cat("\n success \n")
